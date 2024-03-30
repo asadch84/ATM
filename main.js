4a97ff8c2a1cf3e3c8ebe0ab2ce00c35cc2a9823
asadch84 <asadbilalch03@gmail.com>
2024-03-30 11:35:09 +0500

    Ramadan Coding Night , makde program for ATM Machine

---
+++ main.js
@@ -0,0 +1,42 @@
+import inquirer from "inquirer";
+let myBalance = 1000; // Dollar
+let myPin = 1234;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "Enter your Pin Number",
+        type: "number"
+    }
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("Your pin number is correct");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "Please select an option",
+            type: "list",
+            choices: ["Withdraw", "Check Balance"]
+        }
+    ]);
+    if (operationAns.operation === "Withdraw") {
+        // Logic for withdraw
+        console.log("Withdraw operation selected");
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "Enter your amount",
+                type: "number"
+            }
+        ]);
+        myBalance -= amountAns.amount;
+        console.log("Your remaining balance is: " + myBalance);
+    }
+    else if (operationAns.operation === "Check Balance") {
+        // Logic for checking balance
+        console.log("Check balance operation selected");
+        console.log("Your current balance is: " + myBalance);
+    }
+}
+else {
+    console.log("Your pin number is invalid");
+}
